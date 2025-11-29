12dfac3e8fcce5ff562b8777a5c205f5800622a0
David Motorca <dmotorca@pdx.edu>
2025-11-28 22:14:17 -0800

    Added Square Space link and placeholder button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1> MAIN HOME PAGE</h1>
+      <a
+        href="https://app.squareup.com/appointments/book/hh2rhbv6cji0mv/LDAEC7Z67RX1A/start"
+        target="_blank"
+        className="px-4 py-2 bg-pink-500 text-white rounded-xl hover:bg-pink-600"
+      >
+        Book Appointment
+      </a>
     </div>
   );
 }
